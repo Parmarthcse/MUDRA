1e7ca6ccbb78d4de3bb58e1506d8a7e9ff4de99b
unknown <lol.delta123@gmail.com>
2023-02-21 21:52:34 +0100

    coin page start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Home from "./pages/Home";
 import "../src/dist/styles.css";
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Coin from "./pages/Coin";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin" element={<Coin />}>
+          <Route path=":coinId" element={<Coin />}></Route>
+        </Route>
+      </Routes>
     </>
   );
 }
